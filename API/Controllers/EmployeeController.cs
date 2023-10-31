9c4681f14b1bfc2b3bc399b7e5c2e002fdb2673a
edgar <edgargavioli@outlook.com>
2023-10-31 17:15:01 -0300

    API com get e post

---
+++ API/Controllers/EmployeeController.cs
@@ -0,0 +1,34 @@
+﻿using API.Model;
+using API.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmployeeController : ControllerBase //É mais adequado para o uso em APIs
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public EmployeeController(IEmployeeRepository employeeRepository) 
+            => _employeeRepository = employeeRepository;
+        
+        [HttpPost]
+        public IActionResult Add(EmployeeViewModel employeeView)
+        {
+            var employee = new Employee(employeeView.Name, employeeView.Age, null);
+
+            _employeeRepository.Add(employee);
+
+            return Ok();
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var employees = _employeeRepository.Get();
+
+            return Ok(employees);
+        }
+    }
+}
---
+++ API/Infra/ConnectionContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using API.Model;
+
+namespace API.Infra
+{
+    public class ConnectionContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+         => optionsBuilder.UseNpgsql(
+             "Server=localhost;" +
+             "Port=5432;" +
+             "Database=api;" +
+             "User Id=postgres;" +
+             "Password=root;");
+        
+    }
+}
